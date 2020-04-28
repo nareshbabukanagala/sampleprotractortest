aea69a276dcda762f64f49c3d4466e976394223d
nareshbabukanagala <36495628+nareshbabukanagala@users.noreply.github.com>
2020-04-28 21:54:52 +0530

    Update example_spec.js

--- example_spec.js
+++ example_spec.js
@@ -3,7 +3,7 @@ describe('angularjs homepage', function() {
     browser.get('http://www.angularjs.org');
 
     element(by.model('yourName')).sendKeys('Julie');
-
+//
     var greeting = element(by.binding('yourName'));
 
     expect(greeting.getText()).toEqual('Hello Julie!');
